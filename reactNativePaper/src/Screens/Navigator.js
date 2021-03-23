ae9ae0c29b45cd5c0ef8a6fb0410a562a3b60f54
kopro-ov <koprodev@gmail.com>
2021-03-23 18:00:21 +0900

    Tab Navigator layout

---
+++ reactNativePaper/src/Screens/Navigator.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import AppbarHead from '../Components/AppHead';
+
+import Home from './Home';
+import Search from './Search';
+import Trend from './Trend';
+import Shop from './Shop';
+import Mypage from './Mypage';
+
+const Tab = createBottomTabNavigator();
+
+const Navigator = () =>  {
+  return (        
+    <NavigationContainer>
+      <AppbarHead />
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#e91e63"
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Trend"
+          component={Trend}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="chart-bubble" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="shopping"
+          component={Shop}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="shopping" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mypage"
+          component={Mypage}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+            ),
+          }}
+        />        
+      </Tab.Navigator>
+    </NavigationContainer>    
+  )
+};
+
+export default Navigator;
