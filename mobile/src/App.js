c27b4db9a17c77d879c63adfd9349162c7a1186d
kopro-ov <koprodev@gmail.com>
2021-03-31 13:39:56 +0900

    test

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,11 +1,22 @@
-  
-import * as React from 'react';
+// In App.js in a new project
 
-const App = () =>  {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen'
+import DetailsScreen from './screens/DetailsScreen'
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <>
-    </>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Details" component={DetailsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;
