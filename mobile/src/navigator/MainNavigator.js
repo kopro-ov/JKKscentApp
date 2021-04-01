3534f218a75c2d0f16c5f34ff638a3279d8c561c
kopro-ov <koprodev@gmail.com>
2021-04-01 14:45:09 +0900

    g

--- mobile/src/navigator/MainNavigator.js
+++ mobile/src/navigator/MainNavigator.js
@@ -7,67 +7,70 @@ import Community from '../screens/Community/Community';
 import TrendChart from '../screens/TrendChart/TrendChart';
 import Search from '../screens/Search/Search';
 import Mypage from '../screens/Mypage/Mypage';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const MainTab = createBottomTabNavigator();
 
 function MainNavigator() {
   return (
-    <MainTab.Navigator
-      initialRouteName="Feed"
-      activeColor="#f0edf6"
-      inactiveColor="#3e2465"    
-    >
-      <MainTab.Screen
-        name="dashboard"
-        component={Dashboard}
-        options={{
-          tabBarLabel: '홈',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <MainTab.Screen
-        name="trendChart"
-        component={TrendChart}
-        options={{
-          tabBarLabel: '트렌드',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="reddit" color={color} size={26} />
-          ),
-        }}
-      />     
-      <MainTab.Screen
-        name="community"
-        component={Community}
-        options={{
-          tabBarLabel: '커뮤니티',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account-group" color={color} size={26} />
-          ),
-        }}
-      />            
-      <MainTab.Screen
-        name="search"
-        component={Search}
-        options={{
-          tabBarLabel: '검색',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="magnify" color={color} size={26} />
-          ),
-        }}
-      />
-      <MainTab.Screen
-        name="mypage"
-        component={Mypage}
-        options={{
-          tabBarLabel: '마이페이지',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account-circle" color={color} size={26} />
-          ),
-        }}
-      />     
-    </MainTab.Navigator> 
+    <>
+      <MainTab.Navigator
+        initialRouteName="Feed"
+        activeColor="#f0edf6"
+        inactiveColor="#3e2465"    
+      >
+        <MainTab.Screen
+          name="dashboard"
+          component={Dashboard}
+          options={{
+            tabBarLabel: '홈',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <MainTab.Screen
+          name="trendChart"
+          component={TrendChart}
+          options={{
+            tabBarLabel: '트렌드',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="reddit" color={color} size={26} />
+            ),
+          }}
+        />     
+        <MainTab.Screen
+          name="community"
+          component={Community}
+          options={{
+            tabBarLabel: '커뮤니티',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-group" color={color} size={26} />
+            ),
+          }}
+        />            
+        <MainTab.Screen
+          name="search"
+          component={Search}
+          options={{
+            tabBarLabel: '검색',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={26} />
+            ),
+          }}
+        />
+        <MainTab.Screen
+          name="mypage"
+          component={Mypage}
+          options={{
+            tabBarLabel: '마이페이지',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+            ),
+          }}
+        />     
+      </MainTab.Navigator> 
+    </>
   );
 }
 
