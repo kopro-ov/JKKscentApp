eaf9c0bca3c6e18f6e3b1c41b5024b6ce8f8947a
kopro-ov <koprodev@gmail.com>
2021-04-05 15:56:18 +0900

    다음 설문지 추가

--- mobile/src/screens/FragranceFinder/FragranceFinder.js
+++ mobile/src/screens/FragranceFinder/FragranceFinder.js
@@ -1,60 +1,20 @@
 
 import React from 'react';
-import {View, Text, Button} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import FragranceFinderSurvey from '@screens/FragranceFinder/FragranceFinderSurvey';
 
 const Stack = createStackNavigator();
-
-function FragranceFinderHome({ navigation }) {
-  return (    
-    <>
-      <SkeletonPlaceholder>
-        <View style={{ width: "100%", height: 250,  marginBottom: 10 }} />
-      </SkeletonPlaceholder>      
-      <View style={{ width: "80%", marginLeft: "auto", marginRight: "auto" }}>
-        <View style={{ marginTop:30, marginBottom: 20 }}>
-          <Text
-            style={{ fontSize: 20 }}
-          >
-            어떤 향기를{"\n"}찾아 드릴까요?
-          </Text>
-        </View>
-        <View>
-          <Button
-            title="내가 쓰는 향기"
-            onPress={() => {
-              /* 1. Navigate to the Details route with params */
-              navigation.navigate('FragranceFinderSurvey', {
-                itemId: 86,
-                otherParam: 'anything you want here',
-              });
-            }}
-          />
-          <Button
-            title="선물하고 싶은 향기"
-            onPress={() => {
-              /* 1. Navigate to the Details route with params */
-              navigation.navigate('FragranceFinderSurvey', {
-                itemId: 86,
-                otherParam: 'anything you want here',
-              });
-            }}
-          />          
-        </View>
-      </View>
-    </>
-  );
-}
 
 function FragranceFinder() {
 
   return (
     <>
       <Stack.Navigator headerMode={'none'}>
-        <Stack.Screen name="FragranceFinderHome" component={FragranceFinderHome} />
-        <Stack.Screen name="FragranceFinderSurvey" component={FragranceFinderSurvey} />
+        <Stack.Screen
+          name="FragranceFinderSurvey"
+          component={FragranceFinderSurvey} 
+          initialParams={{ itemId: 1 }}
+        />
       </Stack.Navigator>
     </>
   );
--- mobile/src/screens/FragranceFinder/FragranceFinderSurvey.js
+++ mobile/src/screens/FragranceFinder/FragranceFinderSurvey.js
@@ -1,42 +1,81 @@
 
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TouchableOpacity, StyleSheet} from 'react-native';
 import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 
-function FragranceFinderSurvey({ route, navigation }) {
-  /* 2. Get the param */
-  /* https://reactnavigation.org/docs/6.x/params */
-  const { itemId, otherParam } = route.params;
 
+function getSurverData(itemId="1") {
+  const data = require('~/data/fragranceFinderSurver.json');
+  const el = data.find(el => el.itemId === itemId);
+  return el;
+}
+
+function FragranceFinderSurveyButton(props) {
+  console.log(props);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <SkeletonPlaceholder>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <View style={{ width: 60, height: 60, borderRadius: 50 }} />
-          <View style={{ marginLeft: 20 }}>
-            <View style={{ width: 120, height: 20, borderRadius: 4 }} />
-            <View
-              style={{ marginTop: 6, width: 80, height: 20, borderRadius: 4 }}
-            />
-          </View>
-        </View>
-      </SkeletonPlaceholder>      
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('FragranceFinderSurvey', {
-            itemId: Math.floor(Math.random() * 100),
-            otherParam:'???????'
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={props.onPress}
+    >
+      <Text>{props.text}</Text>
+    </TouchableOpacity>    
   );
 }
+
+function FragranceFinderSurvey({ route, navigation }) {
+  const { itemId } = route.params;  
+  surverData = getSurverData(itemId);
+  
+  return (    
+    <>    
+      <SkeletonPlaceholder>
+        <View style={{ width: "100%", height: 250,  marginBottom: 10 }} />
+      </SkeletonPlaceholder>
+      {surverData ? (
+        <View style={styles.container}>
+          <Text style={styles.title}>{surverData.title}</Text>      
+          <View>            
+            {surverData.nextButtons.map((item, index) => (
+              <FragranceFinderSurveyButton
+                text={item.text}
+                key={index}
+                onPress={() => {
+                  navigation.navigate('FragranceFinderSurvey', {
+                    itemId: item.itemId
+                  });
+                }}
+              />
+            ))}          
+          </View>
+        </View>         
+        ) : (
+        <View style={styles.container}>
+          <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+          <Button title="Go back" onPress={() => navigation.goBack()} />         
+        </View>
+        )}
+    </>    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "80%",
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 30,
+    marginBottom: 40
+  },  
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 15,
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
 
 export default FragranceFinderSurvey;
